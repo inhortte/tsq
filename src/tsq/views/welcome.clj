98a1c564aeb87b56907ce5c2c155a48b4328dc0f
Larger One <inhortte@gmail.com>
2012-06-04 23:10:23 -0500

    a few minor deletions

--- src/tsq/views/welcome.clj
+++
@@ -1,9 +0,0 @@
-(ns tsq.views.welcome
-  (:require [tsq.views.common :as common]
-            [noir.content.getting-started])
-  (:use [noir.core :only [defpage]]
-        [hiccup.core :only [html]]))
-
-(defpage "/welcome" []
-         (common/layout
-           [:p "Welcome to tsq"]))
